668c31763ee808bc97f05b0531b5139cd7a14a10
Anisul Mahmud <first.last@tuni.fi>
2023-11-13 22:37:12 +0200

    first try

---
+++ test/add.test.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+
+import add from '../src/add';
+
+describe('add function', () => {
+  it('adds 6 and 4 to equal 10', () => {
+    expect(add(6, 4)).to.equal(10);
+  });
+});
